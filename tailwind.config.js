9a304c1a218319a8de20fe8db161f982f3010fd4
Wade Fletcher <wbfletch@gmail.com>
2021-11-06 13:02:48 -0400

    wip deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mono: ['"Space Mono"', 'monospace'],
+      },
+      colors: {
+        secondary: colors.green,
+        primary: colors.emerald,
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
